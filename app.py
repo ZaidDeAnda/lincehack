097dd2c5b65f7cb77d8fb0bab39b275372863a32
ZaidTheJedi <zaid_hellscream@hotmail.com>
2019-09-13 12:12:12 -0500

    separar bases de datos
    se separaron colecciones de usuarios y orgs

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from db_functions import db_connect, db_insert_user, db_find_all, db_delete_one
 from db_functions import MONGO_URI
 from forms import CreationForm
 
-perfiles = db_connect(MONGO_URI, "LinceHack", "Usuarios")
+perfiles_usuario = db_connect(MONGO_URI, "LinceHack", "Usuarios")
+perfiles_organizacion = db_connect(MONGO_URI, "LinceHack", "Organizaciones")
 
 app = Flask(__name__, template_folder = 'templates')
 
@@ -11,12 +12,12 @@ app = Flask(__name__, template_folder = 'templates')
 def index():
 	return render_template('index.html')
 
-@app.route('/perfiles/<string:name>')
-def perfil(name='nothing'):
+@app.route('/userprofiles/<string:name>')
+def perfil_user(name='nothing'):
 	nombre={
 		'user':name
 	}
-	pe = db_find_all(perfiles, nombre)
+	pe = db_find_all(perfiles_usuario, nombre)
 
 	try:
 		for p in pe:
@@ -27,8 +28,24 @@ def perfil(name='nothing'):
 		print("finally")
 		return render_template('Perfil.html', name=user)
 
-@app.route('/creation/organization', methods=['GET','POST'])
-def creation():
+@app.route('/orgprofiles/<string:name>')
+def perfil_org(name='nothing'):
+	nombre={
+		'user':name
+	}
+	pe = db_find_all(perfiles_organizacion, nombre)
+
+	try:
+		for p in pe:
+			user = p
+		print(user)
+
+	finally:
+		print("finally")
+		return render_template('Perfil.html', name=user)
+
+@app.route('/creation/org', methods=['GET','POST'])
+def creation_org():
 	form = CreationForm(request.form)
 	if request.method == 'POST':
 		Nombre = form.Nombre.data
@@ -42,7 +59,26 @@ def creation():
 				'Descripción': Descripción,
 				'Tags': Tags
 			}
-			db_insert_user(perfiles, Org)
+			db_insert_user(perfiles_organizacion, Org)
+			return redirect('/')
+	return render_template('Register.html')
+
+@app.route('/creation/user', methods=['GET','POST'])
+def creation_user():
+	form = CreationForm(request.form)
+	if request.method == 'POST':
+		Nombre = form.Nombre.data
+		Photo = form.Photo.data
+		Descripción = form.Descripción.data
+		Tags = form.Tags.data
+		if Nombre != None and Photo != None and Descripción != None and Tags != None:
+			Org = {
+				'user': Nombre,
+				'photo': Photo,
+				'Descripción': Descripción,
+				'Tags': Tags
+			}
+			db_insert_user(perfiles_usuario, Org)
 			return redirect('/')
 	return render_template('Register.html')
 if __name__ == '__main__':
