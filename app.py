71a051709633772497d6f42f5ebae407f0bb526f
ZaidTheJedi <zaid_hellscream@hotmail.com>
2019-09-13 10:20:30 -0500

    Mejora general del programa
    Se hizo funcional la obtención de datos desde la base de datos por medio de la URL

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, request, session, redirect
+from db_functions import db_connect, db_insert_user, db_find_all, db_delete_one
+from db_functions import MONGO_URI
+
+perfiles = db_connect(MONGO_URI, "LinceHack", "Usuarios")
 
 app = Flask(__name__, template_folder = 'templates')
 
@@ -6,9 +10,24 @@ app = Flask(__name__, template_folder = 'templates')
 def index():
 	return render_template('index.html')
 
-@app.route('/Perfiles/<name>')
-def perfil(name='Nada'):
-	return render_template('Perfil.html', name=name)
+@app.route('/perfiles/<string:name>')
+def perfil(name='nothing'):
+	nombre={
+		'user':name
+	}
+	pe = db_find_all(perfiles, nombre)
+
+	try:
+		for p in pe:
+			user = p
+		print(user)
+
+	finally:
+		print("finally")
+		return render_template('Perfil.html', name=user)
+
+
+
 
 if __name__ == '__main__':
 	app.run(debug=True, port=5000)
---
+++ db_functions.py
@@ -0,0 +1,26 @@
+from pymongo import MongoClient
+
+MONGO_URI = "mongodb+srv://admin:admin@cluster0-ah6nw.mongodb.net/test"
+
+def db_connect(MONGO_URI, db_name, col_name):
+    client = MongoClient(MONGO_URI)
+    database = client[db_name]
+    collection = database[col_name]
+    return collection
+
+
+def db_insert_user(collection, user):
+    return collection.insert_one(user)
+
+
+def db_find_all(collection, query={}):
+    return collection.find(query)
+
+def db_delete_one(collection, query={}):
+	return collection.delete_many(query)
+
+if __name__ == '__main__':
+
+	bubuc = db_connect(MONGO_URI, 'LinceHack', 'Usuarios')
+	db_insert_user(bubuc, {'user':'bubu'})
+	print("jalo")
