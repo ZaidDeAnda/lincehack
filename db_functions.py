80be2d9acda1c7472a701cdf7a83c52448a4c0b0
ZaidTheJedi <53558127+ZaidTheJedi@users.noreply.github.com>
2019-09-16 08:03:06 -0500

    Update db_functions.py

--- db_functions.py
+++ db_functions.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-MONGO_URI = "mongodb+srv://admin:admin@cluster0-ah6nw.mongodb.net/test"
+MONGO_URI = "mongodb+srv://<user>:<pass>@cluster0-ah6nw.mongodb.net/test"
 
 def db_connect(MONGO_URI, db_name, col_name):
     client = MongoClient(MONGO_URI)
